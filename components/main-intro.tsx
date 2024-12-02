21c3d49fecdc82017cff928dac010e1b034b5782
tanssixyz <130226460+tanssixyz@users.noreply.github.com>
2024-12-02 02:11:32 +0200

    resize hero

--- components/main-intro.tsx
+++ components/main-intro.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardImage, CardTitle } from "./ui/card";
+import Image from "next/image";
 
 export default async function MainIntro() {
   const url = "/about";
@@ -8,7 +9,7 @@ export default async function MainIntro() {
     <div className="border-b-2 max-w-3xl pb-5 ">
       <Card className="flex flex-col rounded-lg shadow-lg p-2">
         <Link href={url}>
-          <CardImage className="h-full mx-auto" image="/heros/hero-1a.jpg" aspectRatio="landscape" role="Image" />
+          <Image src={"/heros/hero-1a.jpg"} width={1000} height={600} alt={"Image"} role="Image"/>      
           <CardHeader>
             <CardTitle><div className="text:2xl lg:text-3xl font-heading">Parisuhdetango</div></CardTitle>
             <CardDescription>Syvennetään vuoropuhelua yhdessä oman kumppanin kanssa</CardDescription>
